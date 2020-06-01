b0a6f8adc1a63fd67306fcb1813bb2db81ee4ba1
Mike Keen <mike@stavvy.com>
2020-06-01 11:16:22 -0400

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rds_data_dao',
-      version='0.1.8',
+      version='0.1.9',
       description='RDS Data API Wrapper',
       url='https://github.com/stavvy/rds-data-dao',
       author='Chris Buonocore',
